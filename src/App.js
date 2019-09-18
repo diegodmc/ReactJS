6d72d145a2f5071c771449b5cd8eb4da499bf9b5
pl5786 <Diego.costa@external.engie.com>
2019-09-18 20:58:56 -0300

    First Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+import './styles.css';
+
+import Header from './components/header/Index.js';
+
+const App = () => (
+  
+    <div className="App">
+      <Header />
+    </div>
+  );
+
+
+export default App;
